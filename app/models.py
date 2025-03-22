0017da5f5961cb1788015ce14b347c92017dfe41
mkuzmenk <mkuzmenko50@gmail.com>
2025-03-22 16:36:00 +0200

    fixes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from datetime import datetime, timezone
 from app import db
 import sqlalchemy as sa
@@ -10,7 +11,7 @@ class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), unique=True, index=True, nullable=False)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), unique=True, index=True, nullable=False)
-    password_hash: so.Mapped[str] = so.mapped_column(sa.String(256), nullable=False)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     #role: so.Mapped[int] = so.mapped_column(sa.SmallInteger, nullable=False)
 
     courses: so.WriteOnlyMapped['Course'] = so.relationship(back_populates='author')
