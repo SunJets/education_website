ec3d47ac2e1483d309ac650f79d6fe26db87a367
mkuzmenk <mkuzmenko50@gmail.com>
2025-03-22 21:56:06 +0200

    save_courses, get_courses

--- app/models.py
+++ app/models.py
@@ -13,8 +13,9 @@ class User(db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     #role: so.Mapped[int] = so.mapped_column(sa.SmallInteger, nullable=False)
 
-    courses: so.WriteOnlyMapped['UserCourse'] = so.relationship(back_populates='author')
+    custom_courses: so.WriteOnlyMapped['UserCourse'] = so.relationship(back_populates='author')
 
+    courses: so.WriteOnlyMapped['Course'] = so.relationship(back_populates='author_course')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -29,13 +30,15 @@ class User(db.Model):
 
 class Course(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, nullable=False)
+    title: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, nullable=False)
     description: so.Mapped[str] = so.mapped_column(sa.Text, index=True, nullable=False)
+    user_ref_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
 
+    author_course: so.Mapped[User] = so.relationship(back_populates='courses')
     user_courses: so.WriteOnlyMapped['UserCourse'] = so.relationship(back_populates='original_course')
 
     def __repr__(self):
-        return f'Course <{self.name}>'
+        return f'Course <{self.title}>'
 
 
 class UserCourse(db.Model):
@@ -47,7 +50,7 @@ class UserCourse(db.Model):
     user_ref_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
     course_ref_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Course.id), index=True)
 
-    author: so.Mapped[User] = so.relationship(back_populates='courses')
+    author_custom_course: so.Mapped[User] = so.relationship(back_populates='custom_courses')
     original_course: so.Mapped[Course] = so.relationship(back_populates='user_courses')
 
     def __repr__(self):
--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,11 @@ from flask import url_for, request, redirect, flash, jsonify, get_flashed_messag
 from app import app
 import sqlalchemy as sa
 from app import db
-from app.models import User
+from app.models import User, Course
 from flask_jwt_extended.exceptions import NoAuthorizationError
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 import re
+from app import jwt
 
 
 @app.route('/api/login', methods=['POST'])
@@ -53,6 +54,82 @@ def register():
     db.session.commit()
 
     return jsonify(username=user.username, email=user.email), 200
+
+
+@app.route('/api/save-courses', methods=['POST'])
+@jwt_required()
+def save_courses():
+    username = get_jwt_identity()
+    user = db.session.scalar(sa.select(User).where(User.username == username))
+
+    if user is None:
+        flash('User is not found')
+        return jsonify(error=get_flashed_messages()[0]), 400
+
+    courses = request.json.get('courses')
+
+    if courses is None:
+        flash('Courses are not found')
+        return jsonify(error=get_flashed_messages()[0]), 400
+
+    add_courses_to_db(courses)
+
+    message = get_flashed_messages()
+    if not message:
+        message = 'success'
+    return jsonify(message=message), 200
+
+
+@app.route('/api/save-courses', methods=['GET'])
+@jwt_required()
+def get_courses():
+    username = get_jwt_identity()
+    user = db.session.scalar(sa.select(User).where(User.username == username))
+
+    if user is None:
+        flash('User is not found')
+        return jsonify(error=get_flashed_messages()[0]), 400
+
+    courses = create_courses_dict(user)
+
+    if courses is None:
+        flash('Courses are not found')
+        return jsonify(error=get_flashed_messages()[0]), 400
+
+    return jsonify(courses=courses), 200
+
+
+@jwt.unauthorized_loader
+def custom_unauthorized_response(callback):
+    return jsonify(error="Unauthorized user"), 401
+
+
+
+
+def add_courses_to_db(course_list):
+    for course in course_list:
+        title = course['title']
+        description = course['description']
+
+        new_course = Course(title=title, description=description)
+        try:
+            db.session.add(new_course)
+            db.session.commit()
+        except:
+            flash(f'''Course "{title}" wasn't added''')
+            db.session.rollback()
+
+
+def create_courses_dict(user):
+    course_list = []
+    courses = db.session.scalars(user.courses.select()).all()
+
+    for course in courses:
+        course_dict = {'title': course.title, 'description': course.description}
+        course_list.append(course_dict)
+
+    return course_list
+
 
 
 def validate_username(username):
