a37be06d6e8535264180410d3a38975e2431dc2d
mkuzmenk <mkuzmenko50@gmail.com>
2025-03-22 15:44:58 +0200

    added authorization

--- app/models.py
+++ app/models.py
@@ -3,3 +3,58 @@ from datetime import datetime, timezone
 from app import db
 import sqlalchemy as sa
 import sqlalchemy.orm as so
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+class User(UserMixin, db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), unique=True, index=True, nullable=False)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), unique=True, index=True, nullable=False)
+    password_hash: so.Mapped[str] = so.mapped_column(sa.String(256), nullable=False)
+    #role: so.Mapped[int] = so.mapped_column(sa.SmallInteger, nullable=False)
+
+    courses: so.WriteOnlyMapped['Course'] = so.relationship(back_populates='author')
+
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'User <{self.username}>'
+
+
+class Course(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, nullable=False)
+    description: so.Mapped[str] = so.mapped_column(sa.Text, index=True, nullable=False)
+
+    user_courses: so.WriteOnlyMapped['UserCourse'] = so.relationship(back_populates='original_course')
+
+    def __repr__(self):
+        return f'Course <{self.name}>'
+
+
+class UserCourse(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, nullable=False)
+    description: so.Mapped[str] = so.mapped_column(sa.Text, index=True, nullable=False)
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    progress: so.Mapped[str] = so.mapped_column(sa.SmallInteger)
+    user_ref_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    course_ref_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Course.id), index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates='courses')
+    original_course: so.Mapped[Course] = so.relationship(back_populates='user_courses')
+
+    def __repr__(self):
+        return f"User's Course <{self.name}>"
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,75 @@
-from flask import url_for, render_template, redirect, flash
+import flask_jwt_extended.exceptions
+from flask import url_for, request, redirect, flash, jsonify, get_flashed_messages
 from app import app
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from flask_jwt_extended.exceptions import NoAuthorizationError
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
-@app.route('/')
-def index():
-    return 'Hi'
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    try:
+        current_user = get_jwt_identity()
+        if current_user:
+            flash('You are already authorized')
+            return jsonify(error=get_flashed_messages()), 400
+
+    except NoAuthorizationError:
+        pass
+
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    user_request = sa.select(User).where(User.username == username)
+    user = db.session.scalar(user_request)
+    if user is None or not user.check_password(password):
+        flash('Invalid username or password')
+        return jsonify(error=get_flashed_messages()), 400
+
+    token = create_access_token(identity=username)
+
+    return jsonify(username=user.username, email=user.email, token=token), 200
+
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    try:
+        current_user = get_jwt_identity()
+        if current_user:
+            return redirect(url_for('index'))
+
+    except NoAuthorizationError:
+        pass
+
+    username = request.json.get('username')
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if not validate_username(username) and not validate_email(email):
+        flash('This user already exists')
+        return jsonify(error=get_flashed_messages()), 400
+
+    user = User(username=username, email=email)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(username=user.username, email=user.email), 200
+
+
+def validate_username(username):
+    user = db.session.scalar(sa.select(User).where(User.username == username))
+    if user is not None:
+        return False
+
+    return True
+
+
+def validate_email(email):
+    user = db.session.scalar(sa.select(User).where(User.email == email))
+    if user is not None:
+        return False
+
+    return True
