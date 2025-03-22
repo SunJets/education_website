77661a359aad6ccb288d4dc54a9b5c6033633979
unknown <larinalexsandr9091@gmail.com>
2025-03-22 16:38:05 +0200

    added getCourses

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,8 @@
         {{ item.text }}
       </div>
     </div>
+
+    <button @click="getCourses">Далі  </button>
   </div>
 </template>
 
@@ -50,6 +52,25 @@ const moveToSelected = (index) => {
 const removeFromSelected = (index) => {
   items.value.push(selectedItems.value[index]);
   selectedItems.value.splice(index, 1);
+};
+
+const getCourses = () => {
+  const tags = selectedItems.value.map(e => e.text);
+
+  fetch('http://localhost:11434/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "model": "deepseek-r1",
+      "prompt": `A person likes these topics: ${tags}. You need to come up with course titles that would be interesting to this person. The course titles should not be too complex, so that you could explain them if needed. Also, each course should have a description of 1-2 sentences. Do not write any text except for the JSON object. The response should contain only a valid JSON string with no extra symbols, spaces, or anything else. The format of the response should be a JSON object like this: {\"courses\":[{\"title\":\"\",\"description\":\"\"}, {\"title\":\"\",\"description\":\"\"}]}.`,
+      "stream": false
+    }),
+  })
+  .then(res => res.json())
+  .then(data => console.log('Response:', data))
+  .catch(error => console.error('Error:', error));
 };
 </script>
 
