2f85b0fb3c8f9193851f4ba09b4c506ac5e4c8c3
unknown <larinalexsandr9091@gmail.com>
2025-03-22 14:49:33 +0200

    cleared

--- app/models.py
+++
@@ -1,5 +0,0 @@
-from typing import Optional
-from datetime import datetime, timezone
-from app import db
-import sqlalchemy as sa
-import sqlalchemy.orm as so
--- app/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import url_for, render_template, redirect, flash
-from app import app
-
-@app.route('/')
-def index():
-    return 'Hi'
